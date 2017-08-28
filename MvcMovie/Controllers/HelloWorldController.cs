485091ae6d50d029fa3c7357d7a8f584ce01c212
shawn <shawndur@buffalo.edu>
2017-08-27 20:14:06 -0400

    basic controller

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller 
+    {
+        //
+        //GET: /HelloWorld/
+        public string Index()
+        {
+            return "Default Action";
+        }
+
+        //
+        //GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
