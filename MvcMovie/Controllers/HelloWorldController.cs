b89a955d9c3b6e2b57f039411e4b0a69014d1d4e
shawn <shawndur@buffalo.edu>
2017-08-27 20:51:04 -0400

    basic views

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -7,16 +7,18 @@ namespace MvcMovie.Controllers
     {
         //
         //GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "Default Action";
+            return View();
         }
 
         //
         //GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
         }
     }
 }
