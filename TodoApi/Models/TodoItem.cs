dc256f1533798d1746c00a10f014a5523915f1dc
shawn <shawndur@buffalo.edu>
2017-09-03 10:12:23 -0400

    added model

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {}
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
