941ea9d6ce29921bab524a9cc6efb1d291e4e43e
shawn <shawndur@buffalo.edu>
2017-09-02 22:12:25 -0400

    added validations and exception handleing

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace RazorPagesMovie.Models
 {
@@ -7,6 +8,8 @@ namespace RazorPagesMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name="Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
